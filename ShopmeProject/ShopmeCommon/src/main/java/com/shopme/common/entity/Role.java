a2c6d0fb88f871feb372459fcb2873e805a56716
AnDT1211 <anthaido.bk@gmail.com>
2024-11-03 01:34:47 -0800

    add User Listing Function

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -67,10 +67,6 @@ public class Role {
 
     @Override
     public String toString() {
-        return "Role{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
+        return "[" + id + " - " + name + "]";
     }
 }
